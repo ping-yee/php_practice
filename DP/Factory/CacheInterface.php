3aae3421e711dad181ae65d38b5b38fef049845f
DESKTOP-KIRGMTS\pingyeeDAGG <s17113225@stu.edu.tw>
2022-07-03 00:37:33 +0800

    Factory init.

--- DP/Factory/CacheInterface.php
+++ DP/Factory/CacheInterface.php
@@ -4,7 +4,21 @@ namespace DP\Factory;
 
 interface CacheInterface 
 {
-    public function set(string $field, string $value): bool;
+
+    /**
+     * 設定快取傳入 key 與 value
+     *
+     * @param string $key
+     * @param string $value
+     * @return boolean
+     */
+    public function set(string $key, string $value): bool;
     
-    public function get(string $field): ?string;
+    /**
+     * 透過 key 取得快取之 value
+     *
+     * @param string $key
+     * @return string|null
+     */
+    public function get(string $key): ?string;
 }
--- DP/Factory/Main.php
+++ DP/Factory/Main.php
@@ -2,20 +2,20 @@
 
 namespace DP\Factory;
 
-require __DIR__ . "/Cache.php";
+require_once __DIR__ . "/Cache.php";
 
-use Cache;
+use DP\Factory\Cache;
 
 $setting = [];
 
-$setting["mysql"]["host"]     = "127.0.0.1";
-$setting["mysql"]["dbname"]   = "test";
-$setting["mysql"]["charset"]  = "UTF8";
-$setting["mysql"]["user"]     = "root";
-$setting["mysql"]["password"] = "root";
+$setting["host"]     = "127.0.0.1";
+$setting["dbname"]   = "test";
+$setting["charset"]  = "utf8mb4";
+$setting["user"]     = "root";
+$setting["password"] = "root";
 
 $cache = new Cache($setting);
 
-$cache->set("foo", "bar");
+// $cache->set("foo", "bar");
 
-print($cache->get("foo"));
+// print($cache->get("foo"));
